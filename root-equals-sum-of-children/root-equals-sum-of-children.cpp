3b6102cabe79bb394d8bc1afd19685b9ef2d8434
sweetpotatoX <67162997+sweetpotatoX@users.noreply.github.com>
2023-06-30 08:24:40 +0300

    Update root-equals-sum-of-children.cpp

--- root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -12,6 +12,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+# include <bits/stdc++.h>
+struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+       TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+     };
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
@@ -23,3 +32,15 @@ public:
         }
     }
 };
+//test
+int main() {
+    
+    TreeNode* root = new TreeNode(10);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(7);
+    Solution solution;
+    bool result = solution.checkTree(root);
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
